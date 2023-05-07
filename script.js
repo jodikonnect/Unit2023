ec8aba9e38ba7acec305e27c9b1d47d7945b4509
Jodi <78118073+jodikonnect@users.noreply.github.com>
2023-05-07 14:39:47 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,59 +1,78 @@
-const walmartJobsUrl = "https://careers.walmart.com/feed?locale=en_US&categories[]=10766&sort=rv.di.dt";
-const airbnbJobsUrl = "https://careers.airbnb.com/api/experiences/search?location=San%20Francisco%2C%20CA&offset=0&limit=5&type=Experiences&metadata=true&orderBy=published_at";
-const twilioJobsUrl = "https://api.twilio.com/2010-04-01/Accounts/YOUR_ACCOUNT_SID/Jobs/LastN.json?PageSize=5";
+window.addEventListener('load', function() {
+  const walmartUrl = 'https://careers.walmart.com/results?q=&page=1&sort=rank&jobCareerArea=all';
+  const twilioUrl = 'https://www.twilio.com/company/jobs';
+  const airbnbUrl = 'https://careers.airbnb.com/positions/';
 
-function getWalmartJobs() {
-  fetch(walmartJobsUrl)
+  const walmartJobs = document.querySelector('#walmart-jobs');
+  const twilioJobs = document.querySelector('#twilio-jobs');
+  const airbnbJobs = document.querySelector('#airbnb-jobs');
+
+  fetch(walmartUrl)
     .then(response => response.text())
     .then(data => {
       const parser = new DOMParser();
-      const xml = parser.parseFromString(data, "application/xml");
-      const items = xml.querySelectorAll("item");
-      let jobs = "";
-      items.forEach(item => {
-        const title = item.querySelector("title").textContent;
-        const link = item.querySelector("link").textContent;
-        jobs += `<li><a href="${link}" target="_blank">${title}</a></li>`;
-      });
-      document.getElementById("walmart-jobs").innerHTML = jobs;
-    });
-}
+      const html = parser.parseFromString(data, 'text/html');
+      const jobs = html.querySelectorAll('.search-result');
 
-function getAirbnbJobs() {
-  fetch(airbnbJobsUrl)
-    .then(response => response.json())
+      let jobList = '';
+      for (let i = 0; i < 5; i++) {
+        const jobTitle = jobs[i].querySelector('.search-result-title').textContent;
+        const jobRecruiter = jobs[i].querySelector('.search-result-recruiter-name').textContent;
+        const jobLocation = jobs[i].querySelector('.search-result-location').textContent;
+        const jobLink = 'https://careers.walmart.com' + jobs[i].querySelector('.search-result-title a').getAttribute('href');
+        jobList += `
+          <li>
+            <a href="${jobLink}" target="_blank">${jobTitle}</a>
+            <p>${jobRecruiter} - ${jobLocation}</p>
+          </li>
+        `;
+      }
+      walmartJobs.innerHTML = jobList;
+    });
+
+  fetch(twilioUrl)
+    .then(response => response.text())
     .then(data => {
-      const items = data.data.experiences;
-      let jobs = "";
-      items.forEach(item => {
-        const title = item.metadata.title;
-        const link = `https://careers.airbnb.com/experiences/${item.id}/`;
-        jobs += `<li><a href="${link}" target="_blank">${title}</a></li>`;
-      });
-      document.getElementById("airbnb-jobs").innerHTML = jobs;
-    });
-}
+      const parser = new DOMParser();
+      const html = parser.parseFromString(data, 'text/html');
+      const jobs = html.querySelectorAll('.job-posting-item');
 
-function getTwilioJobs() {
-  const username = "YOUR_ACCOUNT_SID";
-  const password = "YOUR_AUTH_TOKEN";
-  const headers = new Headers();
-  headers.set("Authorization", "Basic " + btoa(username + ":" + password));
-  fetch(twilioJobsUrl, { headers: headers })
-    .then(response => response.json())
+      let jobList = '';
+      for (let i = 0; i < 5; i++) {
+        const jobTitle = jobs[i].querySelector('.job-title a').textContent;
+        const jobRecruiter = jobs[i].querySelector('.job-department').textContent;
+        const jobLocation = jobs[i].querySelector('.job-location').textContent;
+        const jobLink = jobs[i].querySelector('.job-title a').getAttribute('href');
+        jobList += `
+          <li>
+            <a href="${jobLink}" target="_blank">${jobTitle}</a>
+            <p>${jobRecruiter} - ${jobLocation}</p>
+          </li>
+        `;
+      }
+      twilioJobs.innerHTML = jobList;
+    });
+
+  fetch(airbnbUrl)
+    .then(response => response.text())
     .then(data => {
-      const items = data.jobs;
-      let jobs = "";
-      items.forEach(item => {
-        const title = item.friendly_name;
-        const link = `https://www.twilio.com/console/taskrouter/workspaces/YOUR_WORKSPACE_SID/tasks/${item.sid}`;
-        jobs += `<li><a href="${link}" target="_blank">${title}</a></li>`;
-      });
-      document.getElementById("twilio-jobs").innerHTML = jobs;
-    });
-}
+      const parser = new DOMParser();
+      const html = parser.parseFromString(data, 'text/html');
+      const jobs = html.querySelectorAll('.position-result');
 
-// Call the functions to populate the job listings
-getWalmartJobs();
-getAirbnbJobs();
-getTwilioJobs();
+      let jobList = '';
+      for (let i = 0; i < 5; i++) {
+        const jobTitle = jobs[i].querySelector('.position-title a').textContent;
+        const jobRecruiter = jobs[i].querySelector('.position-team').textContent;
+        const jobLocation = jobs[i].querySelector('.position-location').textContent;
+        const jobLink = 'https://careers.airbnb.com' + jobs[i].querySelector('.position-title a').getAttribute('href');
+        jobList += `
+          <li>
+            <a href="${jobLink}" target="_blank">${jobTitle}</a>
+            <p>${jobRecruiter} - ${jobLocation}</p>
+          </li>
+        `;
+      }
+      airbnbJobs.innerHTML = jobList;
+    });
+});
